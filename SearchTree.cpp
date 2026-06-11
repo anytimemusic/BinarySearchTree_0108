fe0dbbb5b04410090e54e408977c31fb1a6cc1ab
anytimemusic <bima220711@gmail.com>
2026-06-11 11:13:31 +0700

    menambahkan void preorder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -89,3 +89,12 @@ class BinaryTree
             inorder(ptr->rightchild);
         }
     }
+
+    void preorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+    }
