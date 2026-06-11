619feeb053104f94fef48d0568db4975d54a0b13
anytimemusic <bima220711@gmail.com>
2026-06-11 11:00:54 +0700

    search element, parent, currentNode

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -32,4 +32,27 @@ class BinaryTree
     void insert(string element)
     {
         Node *newNode = new Node (element, NULL, NULL);
+        
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node *parent = NULL;
+        Node *currentNode = ROOT;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightchild = newNode;
+        }
     }
